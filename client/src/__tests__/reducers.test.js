7c04fb2a667ade42f1b4520dba5d5ece83bd044b
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-07-13 22:42:45 -0700

    adding TDD with tests folder

---
+++ client/src/__tests__/reducers.test.js
@@ -0,0 +1,13 @@
+//import our actions
+import {
+    UPDATE_PRODUCTS,
+    UPDATE_CATEGORIES,
+    UPDATE_CURRENT_CATEGORY
+} from '../utils/actions';
+
+// create a sample of what our global state will look like
+const initialState = {
+    products: [],
+    categories: [{ name: 'Food'}],
+    currentCategory: '1',
+};
