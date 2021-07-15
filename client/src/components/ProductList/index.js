3a1c3c83b43e31820e68451449be7a263d78889d
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-07-14 19:53:28 -0700

    modifying product list to accept global state

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,23 +1,36 @@
-import React from 'react';
+import React , { useEffect } from 'react';
 import { useQuery } from '@apollo/client';
+
+import { useStoreContext  } from '../../utils/GlobalState';
+import { UPDATE_PRODUCTS } from '../../utils/actions';
 
 import ProductItem from '../ProductItem';
 import { QUERY_PRODUCTS } from '../../utils/queries';
 import spinner from '../../assets/spinner.gif';
 
 function ProductList({ currentCategory }) {
+
+  const [state, dispatch] = useStoreContext();
+
+  const { currentCategory } = state;
+
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-  const products = data?.products || [];
+useEffect(() => {
+  if (data) {
+    dispatch({
+      type: UPDATE_PRODUCTS,
+      products: data.products
+    });
+  }
+}, [data, dispatch]);
 
   function filterProducts() {
     if (!currentCategory) {
-      return products;
+      return state.products;
     }
 
-    return products.filter(
-      (product) => product.category._id === currentCategory
-    );
+    return state.products.filter(product => product.category._id === currentCategory);
   }
 
   return (
