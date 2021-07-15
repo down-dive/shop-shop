9cfd21bda83947370feb446c89b032834ae1c323
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-07-14 17:44:59 -0700

    refactoring categoryMeny compomemt to revieve global state

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
 import { useQuery } from '@apollo/client';
 import { QUERY_CATEGORIES } from '../../utils/queries';
 import { useStoreContext } from '../../utils/GlobalState';
 
 function CategoryMenu({ setCategory }) {
+  
+  const [state, dispatch] = useStoreContext();
+
+  const { categories } = state;
+
   const { data: categoryData } = useQuery(QUERY_CATEGORIES);
-  const categories = categoryData?.categories || [];
+
+  useEffect(() => {
+    // if categoryData exists or has changed from the response of useQuery, then run dispatch()
+    if (categoryData) {
+      // execute our dispatch function with our action object indicating the type of action and the data to set our state for categories to
+      dispatch({
+        type: UPDATE_CATEGORIES,
+        categories: categoryData.categories
+      });
+    }
+  }, [categoryData, dispatch]);
 
   return (
     <div>
