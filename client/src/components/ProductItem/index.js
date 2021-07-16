e9909e4f2f40a2d3806541cf3463ed40b9828985
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-07-15 21:51:59 -0700

    adding multiple similar items in the cart

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -8,11 +8,25 @@ function ProductItem(item) {
 
   const [state, dispatch] = useStoreContext();
 
+  const { cart } = state;
+
   const addToCart = () => {
-    dispatch({
-      type: ADD_TO_CART,
-      product: { ...item, purchaceQuantity: 1 }
-    })
+    // find the cart item with the matching id
+    const itemInCart = cart.find((cartItem) => cartItem._id === _id);
+
+    //if there was a match, call UPDATE with a new purchase quantity
+    if (itemInCart) {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: _id,
+        purchaceQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      })
+    } else {
+      dispatch({
+        type: ADD_TO_CART,
+        product: { ...item, purchaceQuantity: 1 }
+      })
+    }
   }
 
   const {
