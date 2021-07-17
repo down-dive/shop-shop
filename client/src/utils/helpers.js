e6b3cc64404d1c45f591a063af8cf30d98fa5645
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-07-16 18:15:21 -0700

    create idexedDB helper function

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -7,6 +7,25 @@ export function pluralize(name, count) {
 
 export function idbPromise(storeName, method, object) {
   return new Promise((resole, reject) => {
+    // open connection to the databese `shop-shop` with the version of 1
+    const request = window.indexedDB.open('shop-shop', 1);
+
+    // create variables to hold reference to the database, transaction (tx), and object store
+    let db, tx, store;
+
+    // if version has changed (or if this is the first time using the database), run this method and create the three object stores
+    request.onupgradeneeded = function(e) {
+      const db = request.result;
+      //create object store for each type of data and set 'primary' key index to be the `_id ` of the data
+      db.createObjectStore('products', { keyPath: '_id' });
+      db.createObjectStore('categories', { keyPath: '_id' });
+      db.createObjectStore('cart', { keyPath: '_id'});
+    };
+
+    //handle any errors with connecting
+    request.onerror = function(e) {
+      console.log('There was an error');
+    }
 
 
   });
