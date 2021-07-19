5d093977bcc60ccd18f144a0eee5c82b394838d8
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-07-19 16:21:01 -0700

    update cart functionality

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -34,11 +34,18 @@ function Detail() {
         _id: id,
         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
       });
+      // if we are updating quantity, use existing item data and increment purchaceQuantity value by one
+      idbPromise('cart', 'put', {
+        ...itemInCart,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      })
     } else {
       dispatch({
         type: ADD_TO_CART,
         product: { ...currentProduct, purchaseQuantity: 1 }
       });
+      // if porduct isn't in the cart yet, add it to the current shopping cart in IndexedDB
+      idbPromise('cart', 'put', { ...currentProduct, purchaseQuantity: 1 });
     }
   };
 
@@ -47,6 +54,9 @@ function Detail() {
       type: REMOVE_FROM_CART,
       _id: currentProduct._id
     });
+
+    // upon removal from cart, develete the item from IndexedDB using the `currentProduct._id` to lovate what to remove
+    idbPromise('cart', 'delete', { ...currentProduct });
   };
 
 
