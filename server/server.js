185f9c04fd8f6bde3ff35b3230aa2d0573984504
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-07-20 15:38:31 -0700

    deploying

--- server/server.js
+++ server/server.js
@@ -14,6 +14,12 @@ const server = new ApolloServer({
   context: authMiddleware
 });
 
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/shop-shop', {
+//   useFindAndModify: false,
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
+
 server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
